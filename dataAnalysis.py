9af481a2ff86bf078b2d9aa40aa7917b1c6cd017
CobyGoldbergs <CobyGoldbergs@users.noreply.github.com>
2014-09-16 16:48:08 -0400

    analysis code

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,7 +1,7 @@
 f = open('data/data.csv', 'r')
     
 def makeData():
-    dataTable = ""
+    dataTable = ''
     line = f.readline()
     while line:
         dataTable += '<tr>'
@@ -9,4 +9,45 @@ def makeData():
             dataTable += ('<td>' + str(i) + '</td>')
             dataTable += '</tr>'
             line = f.readline()
+    return dataTable
+
+def analyzeData():
+    dataTable = ''
+    k = open('data/data1.csv', 'r')
+    f = open('data/data2.csv', 'w')
+    L = []
+    L2 = []
+    L3 = []
+    Lyear = []
+    line = k.readline()
+    line = k.readline()
+    while line:
+        L.append(line.split(',')[1])
+        Lyear.append(line.split(',')[0])
+        line = k.readline()
+        k.close()
+
+    f.write('Year Interval' + ',' + 'Amount of change between every two years' + '\n')
+    for i in range(1,len(Lyear)):
+        L2.append(int(L[i]) - int(L[i-1]))
+        L3.append(str(Lyear[i-1]) + '-' + str(Lyear[i]))
+        f.write(Lyear[i-1] + '-' + Lyear[i] + ',' + str(int(L[i]) - int(L[i-1])) + '\n')
+        f.close()
+
+    w = open('data2.csv', 'r')
+    line = w.readline()
+    while line:
+        dataTable += '<tr>'
+        for i in line.split(','):
+            dataTable += ('<td bgcolor = "#EBB6FA">' + str(i) + '</td>')
+            dataTable += '</tr>'
+            dataTable += = w.readline()
+            w.close()
+
+    L2 = L2[1:]
+    for i in range(0,len(L2)):
+        if L2[i] >= 0:
+            dataTable += '<p>' + L3[i] + ': ' + (L2[i] * '=') + '</p>'
+        else:
+            dataTable += '<p>' + L3[i] + ': negative' + '</p>'
     return dataTable
